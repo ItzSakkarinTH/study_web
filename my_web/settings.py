9ec4123db01bad7ddc8074447fd159383eef6d48
itzsakkarinTH <itzsakkarinthyt@gmail.com>
2025-05-25 14:25:41 +0700

    setup for deploy

--- my_web/settings.py
+++ my_web/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_5rhxg5mv@=p+9=)&%#kemj2x95k6ttuq&0t=ps-!ih*h+uhd=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
